c2e29b13d5354352045851ad4b96290e85f1813e
Temirbek <tima07853@gmail.com>
2023-01-15 19:42:07 +0300

    Решил Task05

--- Task05/Program.cs
+++ Task05/Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿ Console.Write("Введите чесло: ");
+
+ int number = Convert.ToInt32(Console.ReadLine());
+ if(number > 0)
+ {
+ int count = -number;
+
+ while (count <= number)
+ {
+    Console.Write($"{count} ");
+    count++; // ++ тоже самое что count = count +1
+ }
+ }
+ else
+ {
+    Console.WriteLine("Ошибка, введено отрицательное число");
+ }
