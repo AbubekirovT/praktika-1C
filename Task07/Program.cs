b74cdccf76f498f7f35c29b4aaaef2d301205927
Temirbek <tima07853@gmail.com>
2023-01-15 19:18:27 +0300

    Решил Task07

---
+++ Task07/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите трехзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number > 99 && number < 1000 )
+{
+    int lastDigit = number % 10;
+    Console.WriteLine($"Последней цифрой числа {number} является {lastDigit}");
+}
+else if (number > -1000 && number < -99)
+{
+    int lastDigit = number % 10 *(-1);
+    Console.WriteLine($"Последней цифрой числа {number} является {lastDigit}");
+}
+else
+{
+    Console.WriteLine("Введеное число не трехзначное");
+}
