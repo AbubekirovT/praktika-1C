016e014eaa334f84fd47d3b0d2e6cd592fe11738
Temirbek <tima07853@gmail.com>
2023-01-17 12:43:00 +0300

    update Task16

--- Task16/Program.cs
+++ Task16/Program.cs
@@ -6,20 +6,38 @@
 // 25, 5 -> да
 // 8,9 -> нет
 
-Console.WriteLine("Введите первое число");
-int num1 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите второе число");
-int num2 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите первое число");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите второе число");
+// int num2 = Convert.ToInt32(Console.ReadLine());
 
-if (num1 == num2*num2)
+// if (num1 == num2*num2)
+// {
+//     Console.WriteLine($"{num1} является квадратом {num2}");
+// }
+// else if (num2 == num1*num1)
+// {
+//     Console.WriteLine($"{num2} является квадратом {num1}");
+// }
+// else
+// {
+//     Console.WriteLine("Данные числа не являются квадратами друг друга");
+// }
+// Лишние действия
+
+bool Square(int number1, int number2)
 {
-    Console.WriteLine($"{num1} является квадратом {num2}");
+    return number1 * number1 == number2 || number2 * number2 == number1;
 }
-else if (num2 == num1*num1)
-{
-    Console.WriteLine($"{num2} является квадратом {num1}");
-}
-else
-{
-    Console.WriteLine("Данные числа не являются квадратами друг друга");
-}
+
+Console.Write("Введите число 1: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число 2: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+bool result = Square(number1, number2);
+
+// if (result) Console.WriteLine("да");
+// else Console.WriteLine("нет");
+Console.WriteLine(result ? "да" : "нет");
