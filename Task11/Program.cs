5394c4b63042abe566ee57a9306970b656382c2f
Temirbek <tima07853@gmail.com>
2023-01-17 11:22:43 +0300

    update Task11

--- Task11/Program.cs
+++ Task11/Program.cs
@@ -5,8 +5,19 @@
 // 782 -> 72
 // 918 -> 98
 
-int number = new Random().Next(100,1000);
-int firstDigit = number / 100;
-int thirdDigit = number % 10;
-int num = (firstDigit*10) + thirdDigit;
-Console.WriteLine($"Из числа {number} получилось число {num}");
+int number = new Random().Next(100, 1000);
+// int firstDigit = number / 100;
+// int thirdDigit = number % 10;
+// int num = (firstDigit * 10) + thirdDigit;
+// Console.WriteLine($"Из числа {number} получилось число {num}");
+
+
+int FirstThirdDigits(int num)
+{
+    int firstDigit = num / 100;
+    int thirdDigit = num % 10;
+    int res = (firstDigit * 10) + thirdDigit;
+    return res;
+}
+int result = FirstThirdDigits(number);
+Console.WriteLine($"Из числа {number} получилось число {result}");
