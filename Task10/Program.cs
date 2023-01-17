0b506af54ed3d7284f6e7e788cc079f2aabac67f
Temirbek <tima07853@gmail.com>
2023-01-17 13:58:26 +0300

    update

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -4,13 +4,25 @@
 // 782 -> 8
 // 918 -> 1
 
+// Console.Write("Введите число : ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// if (number < 0) number = number * (-1);
+// if (number > 99 && number < 1000)
+// {
+//     int result = (number / 10) % 10;
+//     Console.WriteLine(result);
+// }
+// else Console.WriteLine("Число не трехзначное");
+
+int Second(int num)
+{
+    int second = (num / 10) % 10;
+    return second;
+}
+
 Console.Write("Введите число : ");
 int number = Convert.ToInt32(Console.ReadLine());
 if (number < 0) number = number * (-1);
-if (number > 99 && number < 1000)
-{
-    int result = (number / 10) % 10;
-    Console.WriteLine(result);
-}
+if (number > 99 && number < 1000) Console.WriteLine(Second(number));
 else Console.WriteLine("Число не трехзначное");
 
---
+++ Task13/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет. Выполнить с помощью числовых операций (целочисленное деление, остаток от деления).
+
+// 645 -> 5
+
+// 78 -> третьей цифры нет
+
+// 32679 -> 6
+
+// Console.Write("Введите число : ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// if (number < 0) number = number * (-1);
+// if (number < 100) Console.WriteLine("третьей цифры нет");
+// else
+// {
+//     while (number > 999)
+//     {
+//         number = number / 10;
+//     }
+//     Console.WriteLine(number % 10);
+// }
+
+int ThreeDigit(int num)
+{
+    while (num > 999)
+    {
+        num = num / 10;
+    }
+    return num;
+}
+
+Console.Write("Введите число : ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 0) number = number * (-1);
+if (number < 100) Console.WriteLine("третьей цифры нет");
+else Console.WriteLine(ThreeDigit(number) % 10);
