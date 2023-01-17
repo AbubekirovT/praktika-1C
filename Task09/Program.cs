6b93881085b82e5aae6114d786e9dda4c80b2248
Temirbek <tima07853@gmail.com>
2023-01-17 10:48:14 +0300

    update Task09

--- Task09/Program.cs
+++ Task09/Program.cs
@@ -5,10 +5,29 @@
 // 12-> 2
 // 85 -> 8
 
+//Решение 1.
+
+// int number = new Random().Next(10,100);
+// Console.WriteLine($"Случайное число из отрезка 10 - 99 => {number}");
+
+// int firstDigit = number / 10;
+// int secondDigit = number % 10;
+// if (firstDigit > secondDigit) Console.WriteLine($"Наибольшая цифра числа => {firstDigit}");
+// else Console.WriteLine($"Наибольшая цифра числа => {secondDigit}");
+
+//Решение через Функцию (Метод)
+
 int number = new Random().Next(10,100);
 Console.WriteLine($"Случайное число из отрезка 10 - 99 => {number}");
 
-int firstDigit = number / 10;
-int secondDigit = number % 10;
-if (firstDigit > secondDigit) Console.WriteLine($"Наибольшая цифра числа => {firstDigit}");
-else Console.WriteLine($"Наибольшая цифра числа => {secondDigit}");
+int maxDigit = MaxDigit(number);
+Console.WriteLine($"Наибольшая цифра числа => {maxDigit}");
+
+int MaxDigit(int num)
+{
+    int firstDigit = num / 10;
+    int secondDigit = num % 10;
+    if (firstDigit > secondDigit) return firstDigit;
+    else return secondDigit;
+}
+
