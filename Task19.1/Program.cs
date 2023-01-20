9f86b4463918178e75b2f3916a0d8df060152c6a
Temirbek <tima07853@gmail.com>
2023-01-20 13:59:36 +0300

    add Task19.1

---
+++ Task19.1/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход число и проверяет, является ли оно палиндромом.
+Console.WriteLine("Введите  число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 0) number = number * (-1);
+double reverse = 0;
+int num = number;
+int degree = 0;
+while (num > 0)
+{
+    num = num / 10;
+    degree++;
+}
+int count = 1;
+while(degree > 0)
+{
+    reverse = reverse + Math.Round((number % Math.Pow(10,count))/Math.Pow(10,count-1),0,MidpointRounding.ToZero) * Math.Pow(10, (degree - 1));
+    degree = degree - 1;
+    count++;
+}
+if (number == reverse) Console.WriteLine($"Число {number} является палиндромом");
+else Console.WriteLine($"Число {number} не является палиндромом");
