b7327a8c2780f20ea5baa7dd8e4d03aa1ce5ca97
Temirbek <tima07853@gmail.com>
2023-01-27 01:02:37 +0300

    add Task25

---
+++ Task25/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+
+// 2, 4 -> 16
+
+Console.WriteLine("Введите число A: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+if (numberB < 1) Console.WriteLine("Число В должно быть натуральным");
+else
+{
+    int res = Exponentiation(numberA, numberB);
+    Console.WriteLine($"{numberA} в степени {numberB} будет {res}");
+}
+
+
+int Exponentiation(int numA, int numB)
+{
+    int result = 1;
+    for (int i = 1; i <= numB; i++) result = result * numA;
+    return result;
+}
