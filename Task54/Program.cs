6936ecfe1a825db1f05034aaff42650fdeffe392
Temirbek <tima07853@gmail.com>
2023-02-10 15:25:35 +0300

    update

--- Task54/Program.cs
+++ Task54/Program.cs
@@ -7,3 +7,65 @@
 // 7 4 2 1
 // 9 5 3 2
 // 8 4 4 2
+
+int[,] GenerateMatrix(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],5} | ");
+            else Console.Write($"{matrix[i, j],5} ");
+        }
+        Console.WriteLine("|");
+    }
+}
+
+int[,] DescendingRows(int[,] matrix)
+{
+
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        for (int k = 0; k < matrix.GetLength(0) - 1; k++)
+        {
+
+
+            for (int i = 0; i < matrix.GetLength(0) - 1; i++)
+            {
+                if (matrix[j, i] < matrix[j, i + 1])
+                {
+                    int max = matrix[j, i + 1];
+                    matrix[j, i + 1] = matrix[j, i];
+                    matrix[j, i] = max;
+                }
+
+            }
+        }
+    }
+
+    return matrix;
+}
+
+
+int[,] mtx = GenerateMatrix(4, 4, 0, 9);
+PrintMatrix(mtx);
+Console.WriteLine();
+DescendingRows(mtx);
+PrintMatrix(mtx);
+
--- Task58/Program.cs
+++ Task58/Program.cs
@@ -5,3 +5,63 @@
 // Результирующая матрица будет:
 // 18 20
 // 15 18
+
+int[,] GenerateMatrix(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],5} | ");
+            else Console.Write($"{matrix[i, j],5} ");
+        }
+        Console.WriteLine("|");
+    }
+}
+
+
+int[,] MatrixMultiplication(int[,] matrix1, int[,] matrix2)
+{
+    int[,] result = new int[matrix1.GetLength(0), matrix1.GetLength(0)];
+    for (int i = 0; i < result.GetLength(0); i++)
+    {
+        for (int j = 0; j < result.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix1.GetLength(1); k++)
+            {
+                result[i, j] += matrix1[i, k] * matrix2[k, j];
+            }
+
+        }
+    }
+    return result;
+}
+
+int[,] mtx1 = GenerateMatrix(2, 3, 0, 9);
+int[,] mtx2 = GenerateMatrix(3, 2, 0, 9);
+if (mtx1.GetLength(1) == mtx2.GetLength(0))
+{
+    int[,] matrixMultiplication = MatrixMultiplication(mtx1, mtx2);
+    PrintMatrix(mtx1);
+    Console.WriteLine();
+    PrintMatrix(mtx2);
+    Console.WriteLine();
+    PrintMatrix(matrixMultiplication);
+}
+else Console.WriteLine("Матрицы нельзя перемножить");
